0fade39e9bafa8c696cff8c0576d3f672bb82b43
Nellbeck <nellbeckfredrik@gmail.com>
2023-09-13 20:38:33 +0200

    Made some changes.

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace ChessBoard
         {
             
             Console.OutputEncoding = System.Text.Encoding.Unicode; // Just a line of code to make the chessboard appear.
-            Console.Write("Skriv in en siffra för hur stort shackbräde du vill ha:"); // Asks the user to typ in an number so the program knows how big chessboard it will make.
+            Console.WriteLine("Skriv in en siffra för hur stort shackbräde du vill ha:"); // Asks the user to typ in an number so the program knows how big chessboard it will make.
             int Number; // Declares an variable
 
             while (true)
@@ -31,18 +31,26 @@ namespace ChessBoard
 
             int[] ANumber = new int[Number]; // Creates an array.
 
+            Console.WriteLine("Hur ska de svarta rutorna se ut?");
+
+            string userInputBlack = Console.ReadLine(); // Asks the user how the black chessboard tiles should look like.
+
+            Console.WriteLine("Hur ska de vita rutorna se ut?");
+
+            string userInputWhite = Console.ReadLine(); // Asks the user how the white chessboard tiles should look like.
+
 
             for (int LoopNumber = 0; LoopNumber < ANumber.Length; LoopNumber++)
             {
                 for (int ArrayNumber = 0; ArrayNumber < ANumber.Length; ArrayNumber++)
                 {
-                    if (ArrayNumber%2 == LoopNumber%2)
+                    if (ArrayNumber % 2 == LoopNumber % 2)
                     {
-                        Console.Write("◻︎");
+                        Console.Write(userInputBlack);
                     }
-                    else 
+                    else
                     {
-                        Console.Write("◼︎");
+                        Console.Write(userInputWhite);
                     }
                 } // Nestled for-loop that creates the number of tiles the user put in.
                 Console.WriteLine();
